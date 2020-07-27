4214f287f23f8b6698f7305b978baa86d2a2941c
j. leandro <jose.morais25@etec.sp.gov.br>
2020-07-26 23:22:42 -0300

    add carousel in product detail page

--- static_in_env/js/scripts.js
+++ static_in_env/js/scripts.js
@@ -1,8 +1,19 @@
 const carousel_destaques = document.getElementById("carouselExampleControls");
-carousel_destaques.children[0].children[0].classList.add("active");
+if (carousel_destaques) {
+  carousel_destaques.children[0].children[0].classList.add("active");
+}
 
 const carousel_imperdiveis = document.getElementById("carouselExampleControls1");
-carousel_imperdiveis.children[0].children[0].classList.add("active");
+if (carousel_imperdiveis) {
+  carousel_imperdiveis.children[0].children[0].classList.add("active");
+}
 
 const carousel_ofertas = document.getElementById("carouselExampleControls2");
-carousel_ofertas.children[0].children[0].classList.add("active");
+if (carousel_ofertas) {
+  carousel_ofertas.children[0].children[0].classList.add("active");
+}
+
+const carousel_vejaTbm = document.getElementById("carouselExampleControls3");
+if (carousel_vejaTbm) {
+  carousel_vejaTbm.children[0].children[0].classList.add("active");
+}
